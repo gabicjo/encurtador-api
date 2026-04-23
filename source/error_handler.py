2dbd9fe777098b076d2cc3090a47e1aace1fbb44
gabicjo <gabriebs@proton.me>
2026-04-23 11:05:15 -0300

    feat: adicionar mensagens de erro mais claras usando try e except

---
+++ source/error_handler.py
@@ -0,0 +1,7 @@
+class URLInvalido(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class SemURL(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
--- source/routes/encurtar_route.py
+++ source/routes/encurtar_route.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from source.services.encurtar_service import create_shortlink
+from source import error_handler
 
 encurtar_bp = Blueprint("encurtar", __name__)
 
@@ -7,8 +8,18 @@ encurtar_bp = Blueprint("encurtar", __name__)
 def generate_code():
     data = request.json
 
-    if "url" in data and data['url'] not in [None, ""]:
-        create_shortlink(data['url'])
-        return jsonify({"message": "ok"})
+    try:
+        if "url" in data:
+            if data['url'] not in [None, ""]:
+                create_shortlink(data['url'])
+                return jsonify({"message": "ok"})
 
-    return jsonify({"message": "error"}), 400
+            raise error_handler.URLInvalido("O URL recebido é invalido")
+
+        raise error_handler.SemURL("URL não foi enviado")
+
+    except error_handler.SemURL:
+        return jsonify({"message": "URL não foi enviado"}), 400
+
+    except error_handler.URLInvalido:
+        return jsonify({"message": "O URL recebido é invalido"}), 400
