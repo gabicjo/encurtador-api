c2936a9c976d19fbdad2abc4eab084c30e7e07d6
gabicjo <gabriebs@proton.me>
2026-04-25 10:55:35 -0300

    refactor: unify BANCO_PATH to globalize variable

--- source/models/encurtar_model.py
+++ source/models/encurtar_model.py
@@ -1,11 +1,9 @@
 import sqlite3
 from source.models import main_model
 
-BANCO_PATH = main_model.BANCO_PATH
-
 
 def save_new_url(url, code):
-    conn = sqlite3.connect(BANCO_PATH)
+    conn = sqlite3.connect(main_model.BANCO_PATH)
     cursor = conn.cursor()
 
     cursor.execute("INSERT INTO links (url, code) VALUES (?, ?)", (url, code,))
--- source/models/redirect_model.py
+++ source/models/redirect_model.py
@@ -1,10 +1,9 @@
 import sqlite3
-
-BANCO_PATH = "banco.db"
+from source.models import main_model
 
 
 def add_new_click(code):
-    conn = sqlite3.connect(BANCO_PATH)
+    conn = sqlite3.connect(main_model.BANCO_PATH)
     cursor = conn.cursor()
 
     cursor.execute("UPDATE links SET clicks = clicks + 1 WHERE code = ?", (code,))
