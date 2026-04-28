16c1af6ea9f4b514d5453ca7401f2498289c797b
gabicjo <gabriebs@proton.me>
2026-04-28 17:13:51 -0300

    feat(auth): integrate LoginManager and auth routes into app
    intent(auth): app needs to initialize LoginManager and register auth blueprint

--- app.py
+++ app.py
@@ -4,16 +4,25 @@ from flasgger import Swagger
 from source.routes.encurtar_route import encurtar_bp
 from source.routes.redirect_route import redirect_bp
 from source.routes.stats_routes import stats_bp
+from source.routes.auth_routes import auth_bp
 from source.models.main_model import criar_tabela
+from source.models.users_model import criar_tabela_users
+from source.auth import login_manager
 
 app = Flask(__name__)
 CORS(app)
 Swagger(app)
 
+# Login Manager
+login_manager.init_app(app)
+
+# Criar tabelas
 criar_tabela()
+criar_tabela_users()
 
 app.register_blueprint(encurtar_bp)
 app.register_blueprint(redirect_bp)
 app.register_blueprint(stats_bp)
+app.register_blueprint(auth_bp)
 
 app.run(port="9284")
